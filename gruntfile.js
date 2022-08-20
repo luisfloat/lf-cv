7addf60c30f07daa2c17d669d0eca09481ab0d9f
Luis Float <contact@luisfloat.com>
2022-08-20 17:05:26 -0300

    feat(build): use puppeteer to render pdf

--- gruntfile.js
+++ gruntfile.js
@@ -1,4 +1,11 @@
 //gruntfile.js
+const puppeteer = require('puppeteer');
+
+const pdfConfig = {
+    src: "file://" + __dirname + "/dist/index.html",
+    dist: "dist/Luis Float (en-us).pdf",
+};
+
 var config = {
     src: 'src/main/**/*.pug',
     dist: 'dist/index.html',
@@ -6,9 +13,20 @@ var config = {
 
 const content = require("./src/main/content/en-us.js");
 
+async function renderPdf() {
+    let done = this.async();
 
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(pdfConfig.src, {
+        waitUntil: 'networkidle2',
+    });
+    await page.pdf({ path: pdfConfig.dist, format: "a4" });
 
+    await browser.close();
 
+    done();
+}
 
 module.exports = function(grunt) {
     grunt.initConfig({
@@ -56,4 +74,5 @@ module.exports = function(grunt) {
     grunt.registerTask('main:styl:compile', [ 'stylus' ]);
     grunt.registerTask('main:watch', [ 'watch' ]);
 
+    grunt.registerTask('main:build', 'Render PDF', renderPdf);
 };
