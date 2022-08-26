84a43bc279d126be097432c8e29991649877773e
Luis Float <contact@luisfloat.com>
2022-08-26 15:25:55 -0300

    feat(build): render multiple htmls in a lang-respective way

--- gruntfile.js
+++ gruntfile.js
@@ -2,46 +2,54 @@
 const puppeteer = require('puppeteer');
 
 const pdfConfig = {
-    src: "file://" + __dirname + "/dist/index.html",
-    dist: "dist/Luis Float (en-us).pdf",
+    src: "file://" + __dirname + "/dist/Luis Float CV ($lang).html",
+    dist: "dist/Luis Float CV ($lang).pdf",
 };
 
 var config = {
+    langs: [ "en-us", "pt-br" ],
     src: 'src/main/**/*.pug',
-    dist: 'dist/index.html',
+    dist: 'dist/Luis Float CV ($lang).html',
 };
 
-const content = require("./src/main/content/en-us.js");
+const vlang = (prop, lang) => prop.replaceAll("$lang", lang);
 
 async function renderPdf() {
     let done = this.async();
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(pdfConfig.src, {
+    await page.goto(vlang(pdfConfig.src, "en-us"), {
         waitUntil: 'networkidle2',
     });
-    await page.pdf({ path: pdfConfig.dist, format: "a4" });
+    await page.pdf({ path: vlang(pdfConfig.dist, "en-us"), format: "a4" });
 
     await browser.close();
 
     done();
 }
 
+function renderHtml() {
+    const pugConfig = {};
+
+    for(let lang of config.langs) {
+        const files = {};
+        files[vlang(config.dist, lang)] = [ 'src/main/index.pug' ];
+
+        pugConfig[lang] = {
+            options: {
+                data: (dest, src) => require(`./src/main/content/${lang}.js`),
+            },
+            files,
+        };
+    }
+
+    return pugConfig;
+}
+
 module.exports = function(grunt) {
     grunt.initConfig({
-        pug: {
-            compile: {
-                options: {
-                    data: function(dest, src) {
-                        return content;
-                    },
-                },
-                files: {
-                    'dist/index.html': [ 'src/main/index.pug' ],
-                },
-            },
-        },
+        pug: renderHtml(),
         stylus: {
             compile: {
                 options: {
