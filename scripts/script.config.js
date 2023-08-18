48f7fe16dfcf3a16e6b86825980b4156e9d75d4f
Luis Float <contact@luisfloat.com>
2023-08-18 15:39:51 -0300

    build: fix and run the scripts in pure node

---
+++ scripts/script.config.js
@@ -0,0 +1,9 @@
+export default {
+    content: {
+        langs: [ "en-us", "pt-br" ],
+    },
+    pdf: {
+        src: "http://localhost:4173/{lang}",
+        dist: "dest/Luis (Float) Emidio CV - {lang}.pdf",
+    },
+};
--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    //root: "src/",
-    //publicDir: "assets/",
     css: {
         preprocessorOptions: {
             stylus: {
@@ -12,7 +10,7 @@ export default defineConfig({
         },
     },
     build: {
-        outDir: "../dist/web/",
+        outDir: "./dist/",
         emptyOutDir: true,
     },
     plugins: [
